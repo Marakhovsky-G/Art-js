1372441d7384d01da7ff0337880c54642e12c33e
Georgy <marakhovskij@gmail.com>
2021-04-27 11:52:47 +0300

    Cделал формы отправки.

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import modals from './modules/modals';
 import sliders from './modules/sliders';
+import forms from './modules/forms';
 
 window.addEventListener('DOMContentLoaded', () => {
   'use stict';
@@ -7,7 +8,7 @@ window.addEventListener('DOMContentLoaded', () => {
   modals();
   sliders('.main-slider-item', 'vertical', '', '');
   sliders('.feedback-slider-item', '', '.main-prev-btn', '.main-next-btn');
-
+  forms();
 
 
 });
